92f425ec7404d9e8ed1625ea6d2100c9401344f2
ViniUndSow <vincentprasse@googlemail.com>
2022-01-25 19:08:27 +0100

    - init

---
+++ src/main/kotlin/vpr/voad/recognitionHelper/RecognitionHelperApplication.kt
@@ -0,0 +1,11 @@
+package vpr.voad.recognitionHelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecognitionHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecognitionHelperApplication>(*args)
+}
---
+++ src/test/kotlin/vpr/voad/recognitionHelper/RecognitionHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package vpr.voad.recognitionHelper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecognitionHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
